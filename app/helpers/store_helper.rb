6d3d5948351bf9cdcf20ab3407e7cdd3648f2b96
i m <ilyabel@MacBook-Pro-Ila.local>
2016-06-13 17:28:45 +0600

    initial commit

---
+++ app/helpers/store_helper.rb
@@ -0,0 +1,10 @@
+module StoreHelper
+	def to_tenge(amount)
+    number_to_currency(
+      amount, 
+      unit: "тг.", 
+      precision: 0, 
+      separator: " ", 
+      format: "%n %u")
+  end 
+end
---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+class Product < ActiveRecord::Base
+  has_many :line_items
+  has_many :orders, through: :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+    #...
+  private
+    # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+    def ensure_not_referenced_by_any_line_item
+        if line_items.empty?
+      return true
+        else
+            errors.add(:base, 'существуют товарные позиции')
+            return false
+        end
+    end
+end
