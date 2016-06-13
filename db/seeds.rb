d290ddb1b1187fc2416cff02571b297553558dce
i m <ilyabel@MacBook-Pro-Ila.local>
2016-06-14 02:23:51 +0600

    no message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(
+  title: "Киото маки",
+  description: "Рис, рыба, капуста, икра",
+  image_url: "kioto-maki.jpg",
+  price: "1000"
+)
+
+Product.create(
+  title: "Ролл",
+  description: "Рис, икра",
+  image_url: "roll-c.jpg",
+  price: "3500"
+)
+
+Product.create(
+  title: "Роллс ролл",
+  description: "Рис, угорь",
+  image_url: "roll-k.jpg",
+  price: "2000"
+)
+
+Product.create(
+  title: "Ролл ветчина",
+  image_url: "roll-l.jpg",
+  description: "Рис, капуста, ветчина",
+  price: "1500"
+)
+
+Product.create(
+  title: "Роял Ролл",
+  description: "Капуста, икра",
+  image_url: "spice-tobic.jpg",
+  price: "2500"
+)
+
+Product.create(
+  title: "Роял Ролл",
+  description: "Капуста, икра",
+  image_url: "spice-tobic.jpg",
+  price: "2500"
+)
