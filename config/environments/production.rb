ed1b88aa0e04851304852b41c8083f4f056f385c
i m <ilyabel@MacBook-Pro-Ila.local>
2016-06-15 20:07:06 +0600

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:           "smtp.gmail.com",
-      port:             "587"
-      domain:           "test.com"
+      port:             "587",
+      domain:           "test.com",
       authentication:   "plain",
       user_name:        "belikbek",
       password:         "belikbek123",
