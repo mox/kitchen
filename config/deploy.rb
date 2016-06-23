f2418b8d1f5f156d3cddd0faadd6b7c2ecc0fce4
i m <ilyabel@MacBook-Pro-Ila.local>
2016-06-24 03:05:52 +0600

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,6 +74,17 @@ namespace :deploy do
       invoke 'puma:restart'
     end
   end
+  	# and run 'cap production deploy:seed' to seed your database
+	desc 'Runs rake db:seed'
+	task :seed => [:set_rails_env] do
+	  on primary fetch(:migration_role) do
+	    within release_path do
+	      with rails_env: fetch(:rails_env) do
+	        execute :rake, "db:seed"
+	      end
+	    end
+	  end
+	end
 
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
