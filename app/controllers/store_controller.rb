6af6e4df0d9dcc1f716792fe2feec1c3db354633
i m <ilyabel@MacBook-Pro-Ila.local>
2016-06-29 22:57:04 +0600

    no message

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
+  after_action :allow_facebook_iframe
 	layout 'home'
+
   
   def index
     # @products = Product.is_public.order(:category)
@@ -10,5 +12,11 @@ class StoreController < ApplicationController
     @sliders = Slider.is_public.order(:weight)
     @reviews = Review.is_public.order(:created_at).limit(4)
   end
+
+  private
+
+    def allow_facebook_iframe
+      response.headers['X-Frame-Options'] = 'ALLOW-FROM http://metrika.yandex'
+    end
   
 end
