a2e3fcf889ab69d0dc793e6b0e47bca6d45988c2
i m <ilyabel@MacBook-Pro-Ila.local>
2016-06-29 22:17:47 +0600

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   YM.counter = "38150710"
+  YM.set_counters :webvisor, :trackLinks
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
