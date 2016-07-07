321bf1c1616658e00d891b04e9c49544541b9715
i m <ilyabel@MacBook-Pro-Ila.local>
2016-07-07 06:37:42 +0600

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :linked_dirs, fetch(:linked_dirs) + %w{public/system public/uploads}
+
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
@@ -34,7 +34,8 @@ set :linked_dirs, fetch(:linked_dirs) + %w{public/system public/uploads}
 
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, fetch(:linked_dirs) + %w{public/system public/uploads}
 namespace :logs do
   desc "tail rails logs" 
   task :tail_rails do
