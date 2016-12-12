16952c8c7365132c24e4859db84e6ce3d1cfbaf4
i m <ilyabel@calculator.local>
2016-12-12 16:53:00 +0300

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ namespace :logs do
   task :tail, :file do |t, args|
     if args[:file]
       on roles(:app) do
-        execute "tail -f #{shared_path}/log/#{args[:file]}.log"
+        execute "tail -f #{release_path}/log/#{args[:file]}.log"
       end
     else
       puts "please specify a logfile e.g: 'rake logs:tail[logfile]"
